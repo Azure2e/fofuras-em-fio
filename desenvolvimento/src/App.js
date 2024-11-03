4ac9a89520f96a04f9f3159638528ea8cdfeeeac
*Levi <*marqueslpa20@gmail.com*>
2024-11-03 16:14:20 -0300

    [FEAT] Primeiro passo

---
+++ desenvolvimento/src/App.js
@@ -0,0 +1,11 @@
+import styles from "./app.module.css"
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className={styles.teste} >Um teste!</h1>
+    </div>
+  );
+}
+
+export default App;
