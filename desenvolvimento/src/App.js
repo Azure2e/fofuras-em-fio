346163258c8f937293f3ffa4c3ea5301be04e9e4
kelySoare5 <kelysoares688@gmail.com>
2024-11-08 20:35:24 -0500

    FEAT: stylized model

--- desenvolvimento/src/App.js
+++ desenvolvimento/src/App.js
@@ -1,9 +1,11 @@
 import styles from "./app.module.css"
+import { Header } from "./components/Header/header";
 
 function App() {
   return (
     <div className="App">
       <h1 className={styles.teste} >Um teste!</h1>
+      <Header />
     </div>
   );
 }
---
+++ desenvolvimento/src/components/Header/header.js
@@ -0,0 +1,10 @@
+import styles from './header.module.css'
+export const Header = () => {
+    return(
+        <section className={styles.exemplo}>
+            <p>Seção header</p>
+
+            <h1>Lembrem de adicionar essa seção no app.js</h1>
+        </section>
+    )
+}
